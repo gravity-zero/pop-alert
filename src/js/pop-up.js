8758bb4727a2169b718f4d91d631dc373fdff5ac
gravity-zero <gravity.neo@gmail.com>
2021-12-02 22:30:08 +0100

    small modif

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,7 +1,7 @@
 class PopUp {
 	
 	constructor() {
-		this.css_file = "https://cdn.pop-up.gravity-zero.fr/assets/css/pop-up2021.css";
+		this.css_file = "../assets/css/pop-up2021.css";
 		this.div_id_master = "pop_master_div";
 		this.div_id_pop = "pop_container";
 		this.open_anim_pop = "pop_container_open";
